2715e55f165542644a3f8c6019c40ecddcfac1c1
RomainTessier <romaintessierpro1@gmail.com>
2024-03-22 19:37:40 +0100

    for list progress

--- vue-car/src/views/BrandView.vue
+++ vue-car/src/views/BrandView.vue
@@ -1,11 +1,23 @@
 <script setup>
-import Cars from '../components/Car.vue'
+import CarList from '../../data/cars.json'
+import { useRoute } from 'vue-router'
 
-let dataCar = Cars.Cars
-let brand = this.$route.params.brand
+let dataCar = CarList.cars
+const route = useRoute()
+const brand = route.params.brand
+
+console.log(brand)
+console.log(dataCar[0].make)
+
+for (let i = 0; i<dataCar.length; i++) {
+    // console.log(dataCar[i].make)
+    if (dataCar[i].make.toUpperCase() == brand.toUpperCase()) {
+        // console.log(dataCar[i])
+    }
+}
 
 </script>
 
 <template>
-    <div class="car" v-for="(brand, index) in dataCar" :key="index"></div>
+    <div class="car" v-for="(brand, index) in dataCar" v-if="dataCar[i].make.toUpperCase() == brand.toUpperCase()" :key="index"></div>
 </template>
